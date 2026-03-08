a078ce063cc8fe2b47777b69d06bae9f5b04053a
Shailendra619 <shailendramandloi619@gmail.com>
2026-03-08 18:52:58 +0530

    xpath path changed

--- src/main/java/pages/ProductPage.java
+++ src/main/java/pages/ProductPage.java
@@ -10,8 +10,8 @@ import utils.WaitUtils;
 
 public class ProductPage extends BasePage {
 
-	By price = By.xpath("//span[@class='a-price-whole']");
-	By addToCart = By.id("add-to-cart-button");
+	By price = By.xpath("((//*[contains(@class, 'a-price-whole')])[1])");
+	By addToCart = By.id("a-autoid-1-announce");
 
 	public String getPrice() {
 
@@ -34,6 +34,8 @@ public class ProductPage extends BasePage {
 			DriverFactory.getDriver().switchTo().window(window);
 
 		}
+	
 
 	}
+	
 }
